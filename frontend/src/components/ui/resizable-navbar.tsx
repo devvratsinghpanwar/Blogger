549e3c8700b4f554efd69a1a92d94b661bda11d1
marcus choudhary <devvratsingh0121@gmail.com>
2025-08-07 19:22:18 +0530

    build commit 2

--- frontend/src/components/ui/resizable-navbar.tsx
+++ frontend/src/components/ui/resizable-navbar.tsx
@@ -57,7 +57,7 @@ export const Navbar = ({ children, className }: NavbarProps) => {
   });
   const [visible, setVisible] = useState<boolean>(false);
 
-  useMotionValueEvent(scrollY, "change", (latest) => {
+  useMotionValueEvent(scrollY, "change", (latest: number) => {
     if (latest > 100) {
       setVisible(true);
     } else {
